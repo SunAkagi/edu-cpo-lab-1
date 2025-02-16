22560c247930e56a1169d53e7b458c133c291ec9
YANGLiang234 <103995785+YANGLiang234@users.noreply.github.com>
2025-02-17 04:41:08 +0800

    Update foo_test.py

--- foo_test.py
+++ foo_test.py
@@ -1,6 +1,6 @@
 import unittest
 from hypothesis import given,  strategies as st
-from foo import BinarySearchTree, TreeNode
+from foo import BinarySearchTree
 
 
 class TestBinarySearchTree(unittest.TestCase):
