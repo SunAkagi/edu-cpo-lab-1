06fba01b06b6830e16d124e16bb12540f10cfa3c
YANGLiang234 <103995785+YANGLiang234@users.noreply.github.com>
2025-02-17 03:42:46 +0800

    Update foo.py

--- foo.py
+++ foo.py
@@ -1,9 +1,117 @@
-class Foo(object):
-    def hello(self):
-        return "hello"
+class TreeNode:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
 
-    def add(self, a, b):
-        return a + b
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+    
+    def insert(self, key):
+        if self.root is None:
+            self.root = TreeNode(key)
+        else:
+            self._insert(self.root, key)
 
-    def sub(self, a, b):
-        return a - b
+    def _insert(self, root, key):
+        if key < root.val:
+            if root.left is None:
+                root.left = TreeNode(key)
+            else:
+                self._insert(root.left, key)
+        elif key > root.val:
+            if root.right is None:
+                root.right = TreeNode(key)
+            else:
+                self._insert(root.right, key)
+
+    def search(self, key):
+        return self._search(self.root, key)
+
+    def _search(self, root, key):
+        if root is None or root.val == key:
+            return root
+        if key < root.val:
+            return self._search(root.left, key)
+        return self._search(root.right, key)
+
+    def delete(self, key):
+        self.root = self._delete(self.root, key)
+
+    def _delete(self, root, key):
+        if root is None:
+            return root
+        
+        if key < root.val:
+            root.left = self._delete(root.left, key)
+        elif key > root.val:
+            root.right = self._delete(root.right, key)
+        else:
+            # Node with only one child or no child
+            if root.left is None:
+                return root.right
+            elif root.right is None:
+                return root.left
+            
+            # Node with two children: Get the inorder successor (smallest in the right subtree)
+            root.val = self._minValueNode(root.right).val
+            # Delete the inorder successor
+            root.right = self._delete(root.right, root.val)
+        
+        return root
+
+    def _minValueNode(self, node):
+        current = node
+        while current.left is not None:
+            current = current.left
+        return current
+
+    def minValue(self):
+        if self.root is None:
+            return None
+        current = self.root
+        while current.left is not None:
+            current = current.left
+        return current.val
+
+    def maxValue(self):
+        if self.root is None:
+            return None
+        current = self.root
+        while current.right is not None:
+            current = current.right
+        return current.val
+
+    def inorder_traversal(self):
+        result = []
+        self._inorder(self.root, result)
+        return result
+
+    def _inorder(self, root, result):
+        if root:
+            self._inorder(root.left, result)
+            result.append(root.val)
+            self._inorder(root.right, result)
+
+    def preorder_traversal(self):
+        result = []
+        self._preorder(self.root, result)
+        return result
+
+    def _preorder(self, root, result):
+        if root:
+            result.append(root.val)
+            self._preorder(root.left, result)
+            self._preorder(root.right, result)
+
+    def postorder_traversal(self):
+        result = []
+        self._postorder(self.root, result)
+        return result
+
+    def _postorder(self, root, result):
+        if root:
+            self._postorder(root.left, result)
+            self._postorder(root.right, result)
+            result.append(root.val)
