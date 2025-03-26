580997f34d8b8e8a80e55a2bee06e9899e1eb1d3
SunAkagi <sunakagi@163.com>
2025-03-26 21:28:55 +0300

    change foo to BST
    change foo to BST

--- BST_test.py
+++ BST_test.py
@@ -1,6 +1,6 @@
 import unittest
 from hypothesis import given,  strategies as st
-from foo import BinarySearchTree
+from BST import BinarySearchTree
 
 
 class TestBinarySearchTree(unittest.TestCase):
