cdd206e9a5b807d36c3599b62c0cbe8145de9246
YANGLiang234 <103995785+YANGLiang234@users.noreply.github.com>
2025-04-08 02:43:27 +0800

    Update BST.py

--- BST.py
+++ BST.py
@@ -1,6 +1,22 @@
 from typing import Optional, Callable, TypeVar, Generic, Tuple, List
 from functools import reduce as functools_reduce
 
+class Comparable(Protocol):
+    def __lt__(self, other: "Comparable") -> bool:
+        ...
+
+    def __gt__(self, other: "Comparable") -> bool:
+        ...
+
+    def __le__(self, other: "Comparable") -> bool:
+        ...
+
+    def __ge__(self, other: "Comparable") -> bool:
+        ...
+
+    def __eq__(self, other: "Comparable") -> bool:
+        ...
+
 KT = TypeVar("KT", bound=Comparable)
 VT = TypeVar("VT")
 S = TypeVar("S")
