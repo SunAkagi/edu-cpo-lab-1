692841826148b953926d8b283a9a060a5dd7e38e
YANGLiang234 <103995785+YANGLiang234@users.noreply.github.com>
2025-04-08 01:22:10 +0800

    Update BST.py

--- BST.py
+++ BST.py
@@ -30,12 +30,8 @@ class KVBinarySearchTree(Generic[KT, VT]):
     def insert(self, key: KT, value: VT) -> None:
         self.root = self._insert(self.root, key, value)
 
-    def _insert(self,
-                node: Optional[KVTreeNode[KT, VT]],
-                key: KT,
-                value: VT
-    ) -> KVTreeNode[KT, VT]:
-
+    def _insert(self, node: Optional[KVTreeNode[KT, VT]], key: KT, value: VT
+               ) -> KVTreeNode[KT, VT]:
         if node is None:
             return KVTreeNode(key, value)
         if key < node.key:
